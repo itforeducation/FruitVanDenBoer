f71dbccea800b87651da7c10b0575ce17df744b1
MatseVH <49431416+Matse2005@users.noreply.github.com>
2023-01-18 09:40:32 +0100

    Added Methods to Fruit class and .gitignore

--- FruitVanDenBoer/Classes/Fruit.cs
+++ FruitVanDenBoer/Classes/Fruit.cs
@@ -2,5 +2,16 @@
 {
     internal class Fruit
     {
+        double price;
+        int amount, maxDiscount = 50, discountAmount = 5;
+
+        public Fruit(int amount, int price)
+        {
+        }
+
+        public double Calculate()
+        {
+            return price;
+        }
     }
 }
