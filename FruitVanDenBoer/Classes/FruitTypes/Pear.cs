bb4ad44c45c32eab02dfc552de18bd98452c61f3
Daan Van Winkel <daan.vanwinkel@ksdiest.be>
2023-01-18 09:27:45 +0100

    gilles suckt

--- FruitVanDenBoer/Classes/FruitTypes/Pear.cs
+++ FruitVanDenBoer/Classes/FruitTypes/Pear.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace FruitVanDenBoer.Classes.FruitTypes
 {
-    internal class Pear
+    internal class Pear : Fruit 
     {
+        public Pear()
+        {
+
+        }
+
     }
 }
